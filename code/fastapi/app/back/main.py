0e459a03d302808939829f3f326c79ece7e87d7f
kzh3010 <zcvpal2@naver.com>
2023-01-27 08:40:09 +0000

    Feat: db

--- code/fastapi/app/back/main.py
+++ code/fastapi/app/back/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import io
-from fastapi import FastAPI, UploadFile, File, Response
+from fastapi import FastAPI, UploadFile, File, Response, HTTPException
 from fastapi.param_functions import Depends
 from pydantic import Json
 from pdf2image import convert_from_bytes
@@ -9,6 +9,9 @@ from pycocotools.coco import COCO
 from mmdet.apis import init_detector
 import numpy as np
 import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
 sys.path.append("/opt/ml/input/code/fastapi/app/back")
 from inference import *
@@ -29,18 +32,59 @@ detector = init_detector(model_config, model_weight, device="cuda:0")
 
 # 요청하는 시험에 대한 정답을 가져오는 부분입니다.
 # TODO: 지훈님께서 만들어준 DB와 연결이 필요합니다.
+Base = declarative_base()
 
 
-def get_answers(exam_info):
-    answer_path = Path(answer_dir, exam_info + ".csv")
-    answer = pd.read_csv(answer_path)
-    return {k: v for k, v in zip(answer["문항번호"], answer["정답"])}
+class Answer(Base):
+    __tablename__ = "answer"
+    id = Column(Integer, primary_key=True, index=True)
+    answer = Column(String, index=True)
+    group = Column(String)
+
+
+engine = create_engine("sqlite:///./answer.db")
+Base.metadata.create_all(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@app.post("/uploadfiles_name/{exam_info}")
+def upload_csv_name(exam_info: str, csv_file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(csv_file.file)
+        question = list(df["question"])
+        answer = list(df["answer"])
+        answer = list(map(str, answer))
+        db = SessionLocal()
+        for a in answer:
+            db.add(Answer(answer=a, group=exam_info))
+        db.commit()
+        db.close()
+        return {"question": question, "answers": answer}
+    except:
+        raise HTTPException(status_code=400, detail="Invalid CSV file")
+
+
+@app.get("/answers/{exam_info}")
+def get_answers(exam_info: str):
+    db = SessionLocal()
+    if db.query(Answer).filter(Answer.group == exam_info).count() > 0:
+        return {"answers": None}
+    else:
+        return {"answers": "No data"}
+
+
+def get_answers_from_db(exam_info):
+    db = SessionLocal()
+    answers = db.query(Answer).filter(Answer.group == exam_info).all()
+    answer = {(i + 1): int(a.answer) for i, a in enumerate(answers)}
+    db.close()
+    return answer
 
 
 # 이미지를 불러와서 모델 예측을 수행하는 부분입니다.
 @app.post("/predict/{exam_info}")
 def predict(exam_info: str, file: UploadFile = File(...)):
-    answer = get_answers(exam_info)
+    answer = get_answers_from_db(exam_info)
     images = convert_from_bytes(file.file._file.read())
     images_np = [np.array(image) for image in images]
     inference = Inference(
