494a6b38920e2382fddb65fc26d081d5dba9def8
JSJSWON <cowdog10@naver.com>
2023-02-06 18:19:58 +0900

    Fix: ui

--- code/fastapi/app/front/main.py
+++ code/fastapi/app/front/main.py
@@ -86,123 +86,145 @@ st.markdown(
 st.sidebar.markdown('<p class="side_title">AI 채점 서비스</p>', unsafe_allow_html=True)
 st.sidebar.caption("보다 간편하게 시험지를 채점하세요")
 st.sidebar.markdown("<hr>", unsafe_allow_html=True)
-categories = ["Guideline", "채점하기"]
+categories = ["Home", "Guideline", "채점하기"]
 select = st.sidebar.selectbox("select a category", categories)
 st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 
 
-def introduce():
+def home():
     st.markdown('<p class="title">몇 점 일 까 ?💯</p>', unsafe_allow_html=True)
     st.markdown(
         '<p class="sub_title">AI 채점 선생님이 당신을 대신해 채점해 드립니다</p>',
         unsafe_allow_html=True,
     )
+    # st.markdown('<p class="preview">P r e v i e w</p>', unsafe_allow_html=True)
+    st.markdown("<p> </p>", unsafe_allow_html=True)
+    empty1, con, empty2 = st.columns([0.3, 0.9, 0.3])
+    with empty1:
+        st.empty()
+    with con:
+        image_comparison(
+            img1="/opt/ml/input/code/fastapi/app/front/explain_img/solve.jpg",
+            img2="/opt/ml/input/code/fastapi/app/front/explain_img/check.jpg",
+            label1="제출된 시험지",
+            label2="Ai채점 결과",
+        )
+    with empty2:
+        st.empty()
+
+
+def introduce():
+    # st.markdown('<p class="title">몇 점 일 까 ?💯</p>', unsafe_allow_html=True)
+    # st.markdown(
+    #     '<p class="sub_title">AI 채점 선생님이 당신을 대신해 채점해 드립니다</p>',
+    #     unsafe_allow_html=True,
+    # )
     # _, con1, con2, _ = st.columns([0.1, 0.2, 0.1, 0.1])
-    _, con1, con2, _ = st.columns([0.15, 0.55, 0.1, 0.1])
-    Guideline = con1.button("**Guideline**")
-    warning = con2.button("**주의사항**")
+    # _, con1, con2, _ = st.columns([0.15, 0.55, 0.1, 0.1])
+    # Guideline = con1.button("**Guideline**")
+    # warning = con2.button("**주의사항**")
+    # st.markdown("<hr>", unsafe_allow_html=True)
+    # if Guideline:
+    st.markdown('<p class="use">G u i d e l i n e</p>', unsafe_allow_html=True)
+    user, res = st.columns([0.5, 0.5])
+    with user:
+        st.markdown(
+            '<div class = "box"><h5 class="use1">How to Use</h5></div>',
+            unsafe_allow_html=True,
+        )
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/use_intro.jpg",
+            width=600,
+            caption="다음 화면에 시험지를 올려주세요",
+        )
+        st.markdown("<p class='explain'>.</p>", unsafe_allow_html=True)
+        st.markdown("<p class='explain'>.</p>", unsafe_allow_html=True)
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/res.jpg",
+            width=600,
+            caption="다운로드 버튼을 클릭",
+        )
+    with res:
+        st.markdown(
+            '<div class = "box"><h5 class="use1">Grading Result</h5></div>',
+            unsafe_allow_html=True,
+        )
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/scoring.jpg",
+            width=580,
+            caption="채점된 결과",
+        )
+        # st.markdown("<hr>", unsafe_allow_html=True)
+        # st.markdown('<p class="preview">P r e v i e w</p>', unsafe_allow_html=True)
+        # st.markdown("<p> </p>", unsafe_allow_html=True)
+        # empty1, con, empty2 = st.columns([0.3, 0.8, 0.3])
+        # with empty1:
+        #     st.empty()
+        # with con:
+        #     image_comparison(
+        #         img1="/opt/ml/input/code/fastapi/app/front/explain_img/solve.jpg",
+        #         img2="/opt/ml/input/code/fastapi/app/front/explain_img/check.jpg",
+        #         label1="제출된 시험지",
+        #         label2="Ai채점 결과",
+        #     )
+        # with empty2:
+        #     st.empty()
+    # if warning:
+    st.markdown('<p class="use2">객 관 식</p>', unsafe_allow_html=True)
+    war_cor_1, war_incor_1 = st.columns([0.5, 0.5])
+    with war_cor_1:
+        st.markdown(
+            '<div class = "box"><h5 class="use2_1">올바른 방법</h5></div>',
+            unsafe_allow_html=True,
+        )
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/y.jpg",
+            width=620,
+            caption='정답 선지에만 "V" 표시를 해주세요',
+        )
+    with war_incor_1:
+        st.markdown(
+            '<div class = "box"><h5 class="use1">틀린 방법</h5></div>',
+            unsafe_allow_html=True,
+        )
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/n1.jpg",
+            width=620,
+            caption='정답을 "O"로 표시하지 말아주세요',
+        )
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/n2.jpg",
+            width=620,
+            caption='틀린 선지에 "X" 또는 "/" 표시를 하지 말아주세요',
+        )
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/n3.jpg",
+            width=620,
+            caption="정답을 선지 번호로 작성하지 말아주세요",
+        )
     st.markdown("<hr>", unsafe_allow_html=True)
-    if Guideline:
-        st.markdown('<p class="use">G u i d e l i n e</p>', unsafe_allow_html=True)
-        user, res = st.columns([0.5, 0.5])
-        with user:
-            st.markdown(
-                '<div class = "box"><h5 class="use1">How to Use</h5></div>',
-                unsafe_allow_html=True,
-            )
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/use_intro.jpg",
-                width=600,
-                caption="다음 화면에 시험지를 올려주세요",
-            )
-            st.markdown("<p class='explain'>.</p>", unsafe_allow_html=True)
-            st.markdown("<p class='explain'>.</p>", unsafe_allow_html=True)
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/res.jpg",
-                width=600,
-                caption="다운로드 버튼을 클릭",
-            )
-        with res:
-            st.markdown(
-                '<div class = "box"><h5 class="use1">Grading Result</h5></div>',
-                unsafe_allow_html=True,
-            )
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/scoring.jpg",
-                width=580,
-                caption="채점된 결과",
-            )
-        st.markdown("<hr>", unsafe_allow_html=True)
-        st.markdown('<p class="preview">P r e v i e w</p>', unsafe_allow_html=True)
-        st.markdown("<p> </p>", unsafe_allow_html=True)
-        empty1, con, empty2 = st.columns([0.3, 0.8, 0.3])
-        with empty1:
-            st.empty()
-        with con:
-            image_comparison(
-                img1="/opt/ml/input/code/fastapi/app/front/explain_img/solve.jpg",
-                img2="/opt/ml/input/code/fastapi/app/front/explain_img/check.jpg",
-                label1="제출된 시험지",
-                label2="Ai채점 결과",
-            )
-        with empty2:
-            st.empty()
-    if warning:
-        st.markdown('<p class="use2">객 관 식</p>', unsafe_allow_html=True)
-        war_cor_1, war_incor_1 = st.columns([0.5, 0.5])
-        with war_cor_1:
-            st.markdown(
-                '<div class = "box"><h5 class="use2_1">올바른 방법</h5></div>',
-                unsafe_allow_html=True,
-            )
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/y.jpg",
-                width=620,
-                caption='정답 선지에만 "V" 표시를 해주세요',
-            )
-        with war_incor_1:
-            st.markdown(
-                '<div class = "box"><h5 class="use1">틀린 방법</h5></div>',
-                unsafe_allow_html=True,
-            )
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/n1.jpg",
-                width=620,
-                caption='정답을 "O"로 표시하지 말아주세요',
-            )
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/n2.jpg",
-                width=620,
-                caption='틀린 선지에 "X" 또는 "/" 표시를 하지 말아주세요',
-            )
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/n3.jpg",
-                width=620,
-                caption="정답을 선지 번호로 작성하지 말아주세요",
-            )
-        st.markdown("<hr>", unsafe_allow_html=True)
-        st.markdown('<p class="use2">주 관 식</p>', unsafe_allow_html=True)
-        war_cor_2, war_incor_2 = st.columns([0.5, 0.5])
-        with war_cor_2:
-            st.markdown(
-                '<div class = "box"><h5 class="use2_1">올바른 방법</h5></div>',
-                unsafe_allow_html=True,
-            )
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/black_square.jpg",
-                width=580,
-                caption="정답에 네모박스를 그려주세요",
-            )
-        with war_incor_2:
-            st.markdown(
-                '<div class = "box"><h5 class="use1">틀린 방법</h5></div>',
-                unsafe_allow_html=True,
-            )
-            st.image(
-                "/opt/ml/input/code/fastapi/app/front/explain_img/black.jpg",
-                width=580,
-                caption="정답만 있는 경우를 주의해 주세요",
-            )
+    st.markdown('<p class="use2">주 관 식</p>', unsafe_allow_html=True)
+    war_cor_2, war_incor_2 = st.columns([0.5, 0.5])
+    with war_cor_2:
+        st.markdown(
+            '<div class = "box"><h5 class="use2_1">올바른 방법</h5></div>',
+            unsafe_allow_html=True,
+        )
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/black_square.jpg",
+            width=580,
+            caption="정답에 네모박스를 그려주세요",
+        )
+    with war_incor_2:
+        st.markdown(
+            '<div class = "box"><h5 class="use1">틀린 방법</h5></div>',
+            unsafe_allow_html=True,
+        )
+        st.image(
+            "/opt/ml/input/code/fastapi/app/front/explain_img/black.jpg",
+            width=580,
+            caption="정답만 있는 경우를 주의해 주세요",
+        )
 
 
 def main():
@@ -248,7 +270,9 @@ def main():
 
 
 if __name__ == "__main__":
-    if select == "Guideline":
+    if select == "Home":
+        home()
+    elif select == "Guideline":
         introduce()
     elif select == "채점하기":
         main()
